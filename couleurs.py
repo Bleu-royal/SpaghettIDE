06962518f7ec3f8703adc69a62f2083499dfaeac
Thomas Lecluse <th.lecluse@gmail.com>
2016-09-15 23:32:19 +0200

    Reconnaissance des balises

--- couleurs.py
+++ couleurs.py
@@ -6,13 +6,15 @@ from PySide.QtCore import *
 class HTMLHighLighter(QSyntaxHighlighter):
     def __init__(self, parent=None):
         super().__init__(parent)  # Appelle le __init__ de la classe parent
+        self.balises = ('HTML', 'BODY', 'HEAD', 'TITLE', 'BR', 'DIV', 'SPAN', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'IMG', 'P')
 
     def select_n_color(self, exp_reguliere, color, text):
         rgx = QRegExp(exp_reguliere)  # Reconnaitre les expressions à colorier
         pos = rgx.indexIn(text, 0)  # Position de la première expression régulière rencontrée
 
         while pos != -1:
-            self.setFormat(pos, rgx.matchedLength(), QColor.fromRgb(color[0], color[1], color[2]))  # Modificateur du texte sélectionné
+            if rgx.capturedTexts()[1].upper() in self.balises:
+                self.setFormat(pos, rgx.matchedLength(), QColor.fromRgb(color[0], color[1], color[2]))  # Modificateur du texte sélectionné
             pos += rgx.matchedLength()  # Déplacer à la fin de l'exp regulière
             pos = rgx.indexIn(text, pos)  # expr suivante
 
