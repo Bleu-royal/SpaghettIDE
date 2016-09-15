70ed1b6ae03188c6435b3218c9f30916d87f9286
Théo Sarrazin <theosarrazin@MacBook-Pro-de-theo.local>
2016-09-15 21:22:22 +0200

    Modification de la Class document

--- document.py
+++ document.py
@@ -1,1 +1,13 @@
 # Module relatif au traitement des documents (noms, extention, sauvegarde, chargement...)
+class Document():
+
+	def __init__(self, textEdit, nom, extension, chemin_enregistrement):
+		#Sauvegarde des variables au seins de la class
+		self.textEdit = textEdit
+		self.nom = nom
+		self.extension = extension
+		self.chemin_enregistrement = chemin_enregistrement
+
+		self.nombre_lignes = self.textEdit.document.lineCount() #Obtention du nombre de lignes presentent dans le QTextEdit
+
+
