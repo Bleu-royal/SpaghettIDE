b9a03149cb0cfbd6899c8748d154b89d97e4cf6f
theo sarrazin <theosarrazin@MacBook-Pro-de-theo.local>
2016-09-15 13:57:45 +0200

    Ajout de la partie graphique

--- couleurs.py
+++ couleurs.py
@@ -1,1 +1,32 @@
 # Module relatif au traitement des couleurs
+from PySide.QtGui import *
+from PySide.QtCore import *
+
+class HTMLHighLighter(QSyntaxHighlighter):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def highlightBlock(self, text):
+        rgx = QRegExp("<([\w|/|\s|=|\"|\.]+)>")
+        pos = rgx.indexIn(text, 0)
+
+        while pos != -1:
+            self.setFormat(pos, rgx.matchedLength(), QColor.fromRgb(255, 0, 0))
+            pos += rgx.matchedLength()
+            pos = rgx.indexIn(text, pos)
+
+        rgx = QRegExp("\"(\w|\.)+\"")
+        pos = rgx.indexIn(text, 0)
+
+        while pos != -1:
+            self.setFormat(pos, rgx.matchedLength(), QColor.fromRgb(0, 255, 0))
+            pos += rgx.matchedLength()
+            pos = rgx.indexIn(text, pos)
+
+        rgx = QRegExp(">(\w|\.|\s)+<")
+        pos = rgx.indexIn(text, 0)
+
+        while pos != -1:
+            self.setFormat(pos + 1, rgx.matchedLength() - 2, QColor.fromRgb(0, 0, 255))
+            pos += rgx.matchedLength()
+            pos = rgx.indexIn(text, pos)
--- graphique.py
+++ graphique.py
@@ -1,1 +1,37 @@
 # Module relatif à l'interface graphique
+import sys
+from PySide.QtGui import *
+from PySide.QtCore import *
+from PySide.QtWebKit import *
+from couleurs import *
+
+app = QApplication(sys.argv)
+
+class Fenetre(QWidget):
+    def __init__(self, titre, size=[600, 500]):
+        super().__init__()
+        self.setWindowTitle(titre)
+        self.resize(size[0], size[1])
+
+        self.layout = QHBoxLayout()
+
+        self.edit = QTextEdit()
+        self.layout.addWidget(self.edit)
+
+        self.button = QPushButton("Valider")
+        self.layout.addWidget(self.button)
+
+        self.nav = QWebView()
+        self.nav.load("http://www.google.fr")
+        self.layout.addWidget(self.nav)
+
+        self.setLayout(self.layout)
+
+        self.show()
+
+        editor = QTextEdit()
+        self.highlighter = HTMLHighLighter(self.edit.document())
+
+
+fenetre = Fenetre("IDE de la mort qui tue (Bleu Royal)", [400, 400])
+sys.exit(app.exec_())
