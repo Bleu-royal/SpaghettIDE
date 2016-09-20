d17e0bb2d72bb08ff3d7e4a945af2c4487cac0a9
Théo Sarrazin <theosarrazin@MacBook-Pro-de-theo.local>
2016-09-20 21:44:08 +0200

    MAJ de la gestion des couleurs

--- couleurs.py
+++ couleurs.py
@@ -7,21 +7,36 @@ from PySide.QtCore import *
 class HTMLHighLighter(QSyntaxHighlighter):
     def __init__(self, parent=None):
         super().__init__(parent)  # Appelle le __init__ de la classe parent
-        self.balises = ('HTML', 'BODY', 'HEAD', 'TITLE', 'BR', 'DIV', 'SPAN', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'IMG', 'P')
-        self.attributs = ('src', 'href', 'class', 'id', 'charset', 'rel')
+        self.balises = ('HTML', 'META', 'LINK', 'FORM', 'A', 'INPUT', 'STYLE', 'SCRIPT', 'BODY', 'HEAD', 'NAV', 'TITLE', 'BR', 'DIV', 'SPAN', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'IMG', 'P', 'UL', 'LI', 'BUTTON')
+        self.attributs = ('SRC', 'HREF', 'CLASS', 'ID', 'CHARSET', 'REL', 'TITLE', 'TYPE', 'NAME', 'VALUE')
 
-    def select_n_color(self, exp_reguliere, color, text):
+    def select_n_color(self, exp_reguliere, color, text, word=False):
         text = text.upper()
         rgx = QRegExp(exp_reguliere)  # Reconnaitre les expressions à colorier
         pos = rgx.indexIn(text, 0)  # Position de la première expression régulière rencontrée
 
         while pos != -1:
-            self.setFormat(pos, rgx.matchedLength(),QColor.fromRgb(color[0], color[1], color[2]))  # Modificateur du texte sélectionné
+            if word:
+                start = self.place_in_block(text,word,pos)
+                self.setFormat(start, len(word),QColor.fromRgb(color[0], color[1], color[2]))  # Modificateur du texte sélectionné
+            else:
+                self.setFormat(pos, rgx.matchedLength(),QColor.fromRgb(color[0], color[1], color[2]))  # Modificateur du texte sélectionné
             pos += rgx.matchedLength()  # Déplacer à la fin de l'exp regulière
             pos = rgx.indexIn(text, pos)  # expr suivante
 
+    def place_in_block(self,block, word, pos):
+
+        for i in range(pos, len(block)):
+            if block[i:i+len(word)] == word:
+                return i
+        return False
+
     def highlightBlock(self, text):
+        for e in self.attributs:
+            self.select_n_color("<(.)*%s(.)*>"%e, (255, 255, 0), text, e)  # Balises
+
+        self.select_n_color("\"(\w|\.|\s|\+|/|-|,|#)+\"", (40, 200, 40), text)  # Guillemets
 
         for e in self.balises:
-            self.select_n_color("<(/)*%s>"%e, (255, 0, 0), text)  # Balises
-        self.select_n_color("\"(\w|\.)+\"", (40, 200, 40), text)  # Guillemets
+            self.select_n_color("<(/)*%s(\w|\s|:|;|=|\"|\d|\.|/|\+|-|,|#)*>"%e, (255, 0, 0), text, e)  # Balises
+
