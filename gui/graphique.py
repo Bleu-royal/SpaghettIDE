852d2856cfab7af54109e889732de52ab54c94df
Théo Sarrazin <theosarrazin@MacBook-Pro.local>
2016-11-04 08:19:04 +0100

    Maj QSplitter de la mort qui tue

--- gui/graphique.py
+++ gui/graphique.py
@@ -156,7 +156,7 @@ class Fenetre(QWidget):
         self.splitter = QSplitter()
         self.splitter.addWidget(self.img_treeview_splitter)
         self.splitter.addWidget(self.tab_widget)
-        self.splitter.setSizes([100,200])
+        self.splitter.setSizes([100,400])
 
         # Bouton temporaire d'ouverture d'un fichier
         self.ouvrir = QPushButton("Ouvrir") 
