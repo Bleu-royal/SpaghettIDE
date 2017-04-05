06f3123aaecc8b855d17f6aec37ab334d776c344
Théo Sarrazin <theo@sarrazin.pro>
2017-04-06 01:12:25 +0200

    Ajout de fichier texte

--- kernel/variables.py
+++ kernel/variables.py
@@ -3,7 +3,7 @@ extension_by_language = {"c": ["*c", "*h"], "python": ["*py"], "": ["*c", "*h", 
 supported_extensions = {"c": "C", "h": "C", "py": "Python", "jpg": "jpg", "png": "png", "gif": "gif", "txt": "txt"}
 
 imgs_extentions = ["*jpg", "*png"]
-txt_extentions_filedialog = "Fichier Text (*.txt)"
-txt_extentions = ["*txt"]
+txt_extentions_filedialog = "Fichier Text (*.txt);;Fichier Json (*.json);;Fichier XML (*.xml)"
+txt_extentions = ["*txt", "*json", "*xml"]
 gif_extentions = ["*gif"]
 ext_neutres = txt_extentions + imgs_extentions + gif_extentions
