5c3213cdd4240e98f3717bb47582716dee323789
Théo Sarrazin <theo@sarrazin.pro>
2017-04-05 10:13:08 +0200

    Ajout du lexer python

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,27 +1,35 @@
-from lexer import c, ar
+from lexer import c, python, ar
 
 
 def lexing(language, word):
     if language in ("c", "h"):
         return c.lexing(word)
+    elif language in ("py", ):
+        return python.lexing(word)
     else:
         return []
 
 def tokenize(language, data):
     if language in ("c", "h"):
         return c.tokenize(data)
+    elif language in ("py", ):
+        return python.tokenize(data)
     else:
         []
 
 def colorate(language, data):
     if language in ("c", "h"):
         return c.colorate(data)
+    elif language in ("py", ):
+        return python.colorate(data)
     else:
         return []
 
 def update_token_color(language):
     if language in ("c", "h"):
         c.update_token_color()
+    elif language in ("py", ):
+        python.update_token_color()
 
 
 def yaccing(language, data, get_errros=True):
