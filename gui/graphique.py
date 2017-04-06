e196f2f156a096cbbb7c8f2a4e04c4860d6bb6e4
Théo Sarrazin <theo@sarrazin.pro>
2017-04-06 12:37:51 +0200

    MAJ

--- gui/graphique.py
+++ gui/graphique.py
@@ -490,7 +490,7 @@ class Fenetre(QWidget):
         self.project_path = ""
 
         chemin = QFileDialog.getExistingDirectory(self, get_text("chg_worplace"), self.workplace_path) + "/"
-        if chemine != "/":
+        if chemin != "/":
             self.treeview.change_worplace(chemin)
             self.workplace_path = chemin
             write_xml("conf.xml", "current_workplace", chemin)
